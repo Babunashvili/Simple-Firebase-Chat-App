35eb0bcf15d0678ef906f38282dfb5d07c22494e
david-babunashvili <babunashvilidato@gmail.com>
2017-01-28 22:09:58 +0400

    cleared files

--- js/app.js
+++ js/app.js
@@ -1,53 +1,51 @@
-$(document).ready(function(){
-  var rootRef = firebase.database().ref();
-  var currentMessageRef = rootRef.child('currentMessage');
-  var add = $('.add');
-  var detele = $('.delete');
+$(document).ready(function() {
+	var rootRef = firebase.database().ref();
+	var currentMessageRef = rootRef.child('currentMessage');
+	var add = $('.add');
+	var detele = $('.delete');
 
-  function createNewComment(postId, username, text) {
-    firebase.database().ref('messages').push({
-      id:postId,
-      text: text,
-      author: username
-    });
-    $('.message').val('');
-  }
+	function createNewComment(postId, username, text) {
+		firebase.database().ref('messages').push({
+			id: postId,
+			text: text,
+			author: username
+		});
+		$('.message').val('');
+	}
 
-  var starCountRef = firebase.database().ref('messages');
+	var starCountRef = firebase.database().ref('messages');
 
-  firebase.database().ref('/messages').on('value',function(snap){
-    $('#msg').html('');
-    snap.forEach(function(noteSnapshot) {
-          $('#msg').append("<li><b>"+noteSnapshot.val().author+"</b>: "+noteSnapshot.val().text+"</li>");
-        });
-    $('.mess').animate({ scrollTop: $('#msg').height() }, 50);
-  });
+	firebase.database().ref('/messages').on('value', function(snap) {
+		$('#msg').html('');
+		snap.forEach(function(noteSnapshot) {
+			$('#msg').append("<li><b>" + noteSnapshot.val().author + "</b>: " + noteSnapshot.val().text + "</li>");
+		});
+		$('.mess').animate({
+			scrollTop: $('#msg').height()
+		}, 50);
+	});
 
-  add.on('click',function(){
-    var d = new Date();
-    var n = d.getMilliseconds();
-    if ($('.name').val().length > 0 && $('.message').val().length > 0) {
-     createNewComment(n, $('.name').val(),$('.message').val());
-   }
- });
- 
-  $(".message").keyup(function(event){
-    if (event.keyCode == 13) {        
-      if(event.shiftKey){
-        event.stopPropagation();
-      } else {
-        event.preventDefault();
-        var d = new Date();
-        var n = d.getMilliseconds();
+	add.on('click', function() {
+		var d = new Date();
+		var n = d.getMilliseconds();
+		if ($('.name').val().length > 0 && $('.message').val().length > 0) {
+			createNewComment(n, $('.name').val(), $('.message').val());
+		}
+	});
 
-        if ($('.name').val().length > 0 && $('.message').val().length > 0) {
-         createNewComment(n, $('.name').val(),$('.message').val());
-       }
+	$(".message").keyup(function(event) {
+		if (event.keyCode == 13) {
+			if (event.shiftKey) {
+				event.stopPropagation();
+			} else {
+				event.preventDefault();
+				var d = new Date();
+				var n = d.getMilliseconds();
 
-     }
-   }
- });
- 
-// starCountRef.onDisconnect().remove();
-
+				if ($('.name').val().length > 0 && $('.message').val().length > 0) {
+					createNewComment(n, $('.name').val(), $('.message').val());
+				}
+			}
+		}
+	});
 });
